b663293b6b11dd0777c90af225ed6898ffc96d3d
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-07-11 10:23:13 +0300

    feat: add category-section

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,16 @@
-const brandsSwiper = new Swiper("#brands-swiper", {
-	slidesPerView: 6,
-	slidesPerColumn: 2,
-	grid: {
-			rows: 2,
-		},
-	spaceBetween: 30,
-	navigation: {
-		nextEl: ".main-brands__arrows-next",
-		prevEl: ".main-brands__arrows-prev",
-	},
-	pagination: {
-		el: ".main-brands__bullets",
-		clickable: true,
-	},
+const brandsSwiper = new Swiper('#brands-swiper', {
+  slidesPerView: 6,
+  slidesPerColumn: 2,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.main-brands__arrows-next',
+    prevEl: '.main-brands__arrows-prev',
+  },
+  pagination: {
+    el: '.main-brands__bullets',
+    clickable: true,
+  },
 });
-
