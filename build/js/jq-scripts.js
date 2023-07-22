7d5587949fd4da3921dfc373f8cbd7f57336ad0b
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-07-22 17:20:11 +0300

    feat: add modal, refactor form, hover brands icons

--- build/js/jq-scripts.js
+++ build/js/jq-scripts.js
@@ -10,32 +10,33 @@ function changeCheckedInputForm(elem) {
   }
 }
 
-function changeDisableFormSubmitButton() {
+function changeDisableFormSubmitButton(elem) {
   let isRes = true;
-  $('.main-main__search-form-select').each(function () {
+
+  elem.find('.main-main__search-form-select').each(function () {
     if ($(this).attr('data-checked') === 'off') {
       isRes = false;
     }
   });
 
   if (isRes) {
-    $('.main-main__search-form-btn-search').removeAttr('disabled');
+    elem.find('.main-main__search-form-btn-search').removeAttr('disabled');
   } else {
-    $('.main-main__search-form-btn-search').attr('disabled', 'disabled');
+    elem.find('.main-main__search-form-btn-search').attr('disabled', 'disabled');
   }
 }
 
-function changeDisableFormResetButton() {
+function changeDisableFormResetButton(elem) {
   let isRes = true;
-  $('.main-main__search-form-select').each(function () {
+  elem.find('.main-main__search-form-select').each(function () {
     if ($(this).attr('data-checked') === 'on') {
       isRes = false;
     }
   });
   if (!isRes) {
-    $('.main-main__search-form-btn-reset').removeAttr('disabled');
+    elem.find('.main-main__search-form-btn-reset').removeAttr('disabled');
   } else {
-    $('.main-main__search-form-btn-reset').attr('disabled', 'disabled');
+    elem.find('.main-main__search-form-btn-reset').attr('disabled', 'disabled');
   }
 }
 
@@ -54,105 +55,126 @@ function addActive(x) {
 }
 // ----------------------------------------------------------------//
 
-$('.main-main__search-form-select').on('click', function () {
-  if ($(this).hasClass('active')) {
-    $(this).removeClass('active');
-    $(this).find('.main-main__search-form-select-datalist').fadeOut(1000);
-  } else {
-    $('.main-main__search-form-select').removeClass('active');
-    $('.main-main__search-form-select-datalist').fadeOut(1000);
-    $(this).addClass('active');
-    $(this).find('.main-main__search-form-select-datalist').fadeIn(1000);
-  }
+$('.main-main__search-form').each(function () {
+  $(this)
+    .find('.main-main__search-form-select')
+    .on('click', function () {
+      if ($(this).hasClass('active')) {
+        $(this).removeClass('active');
+        $(this).find('.main-main__search-form-select-datalist').fadeOut(1000);
+      } else {
+        $('.main-main__search-form-select').removeClass('active');
+        $('.main-main__search-form-select-datalist').fadeOut(1000);
+        $(this).addClass('active');
+        $(this).find('.main-main__search-form-select-datalist').fadeIn(1000);
+      }
+
+      $(this)
+        .find('.main-main__search-form-select-input')
+        .on('input', function () {
+          const text = $(this).val().toUpperCase();
+          $(this)
+            .next()
+            .find('option')
+            .each(function () {
+              if ($(this).val().toUpperCase().indexOf(text) > -1) {
+                $(this).css('display', 'block');
+              } else {
+                $(this).css('display', 'none');
+              }
+            });
+        });
+    });
+
+  $(this)
+    .find('.main-main__search-form-select')
+    .each(function (index) {
+      $(this)
+        .find('option')
+        .each(function () {
+          $(this).on('click', function () {
+            $(this).parent().prev().val($(this).val());
+            $(this).parent().fadeOut();
+            changeCheckedInputForm($(this));
+            changeDisableFormSubmitButton($(this).parent().parent().parent());
+            changeDisableFormResetButton($(this).parent().parent().parent());
+
+            if (
+              index !== 5 &&
+              $(this).parent().parent().next().css('display') !== 'flex' &&
+              $(this).parent().parent().next().attr('data-checked') === 'off'
+            ) {
+              $(this).parent().parent().next().slideToggle(500).fadeIn();
+            }
+          });
+        });
+    });
+
+  $(this)
+    .find('.main-main__search-form-select')
+    .find('button')
+    .each(function () {
+      $(this).on('click', function (event) {
+        event.preventDefault();
+        $(this).parent().prev().val('');
+
+        $(this)
+          .parent()
+          .find('option')
+          .each(function () {
+            $(this).css('display', 'block');
+          });
+
+        $(this).parent().fadeIn(1000);
+        changeCheckedInputForm($(this));
+        changeDisableFormSubmitButton($(this).parent().parent().parent());
+        changeDisableFormResetButton($(this).parent().parent().parent());
+      });
+    });
+
+  $(this)
+    .find('.main-main__search-form-btn-reset')
+    .each(function () {
+      $(this).on('click', function () {
+        $(this)
+          .parent()
+          .parent()
+          .find('.main-main__search-form-select')
+          .each(function () {
+            $(this).attr('data-checked', 'off');
+            $(this).find('.main-main__search-form-select-input').val('');
+          });
+        changeDisableFormSubmitButton($(this).parent().parent().parent());
+        changeDisableFormResetButton($(this).parent().parent().parent());
+
+        $(this)
+          .parent()
+          .parent()
+          .find('.main-main__search-form-select')
+          .each(function (index) {
+            if (index !== 0) {
+              $(this).slideUp().fadeOut();
+            }
+          });
+      });
+    });
 
   $(this)
     .find('.main-main__search-form-select-input')
-    .on('input', function () {
-      const text = $(this).val().toUpperCase();
-      $(this)
-        .next()
-        .find('option')
-        .each(function () {
-          if ($(this).val().toUpperCase().indexOf(text) > -1) {
-            $(this).css('display', 'block');
-          } else {
-            $(this).css('display', 'none');
-          }
-        });
-    });
-});
-
-$('.main-main__search-form-select').each(function (index) {
-  $(this)
-    .find('option')
-    .each(function () {
-      $(this).on('click', function () {
-        $(this).parent().prev().val($(this).val());
-        $(this).parent().fadeOut();
-        changeCheckedInputForm($(this));
-        changeDisableFormSubmitButton();
-        changeDisableFormResetButton();
-
-        if (
-          index !== 5 &&
-          $(this).parent().parent().next().css('display') !== 'flex' &&
-          $(this).parent().parent().next().attr('data-checked') === 'off'
-        ) {
-          $(this).parent().parent().next().slideToggle(500).fadeIn();
+    .on('keydown', function (event) {
+      if (event.keyCode === 40) {
+        currentFocus += 1;
+        addActive($(this).find('option'));
+      } else if (event.keyCode === 38) {
+        currentFocus -= 1;
+        addActive($(this).find('option'));
+      } else if (event.keyCode === 13) {
+        event.preventDefault();
+        if (currentFocus > -1) {
+          if ($(this).find('option')) $(this).find('option')[currentFocus].click();
         }
-      });
+      }
     });
-});
-
-$('.main-main__search-form-select')
-  .find('button')
-  .each(function () {
-    $(this).on('click', function (event) {
-      event.preventDefault();
-      $(this).parent().prev().val('');
-
-      $(this)
-        .parent()
-        .find('option')
-        .each(function () {
-          $(this).css('display', 'block');
-        });
-
-      $(this).parent().fadeIn(1000);
-      changeCheckedInputForm($(this));
-      changeDisableFormSubmitButton();
-      changeDisableFormResetButton();
-    });
-  });
-
-$('.main-main__search-form-btn-reset').on('click', () => {
-  $('.main-main__search-form-select').each(function () {
-    $(this).attr('data-checked', 'off');
-    $(this).find('.main-main__search-form-select-input').val('');
-  });
-  changeDisableFormSubmitButton();
-  changeDisableFormResetButton();
-
-  $('.main-main__search-form-select').each(function (index) {
-    if (index !== 0) {
-      $(this).slideUp().fadeOut();
-    }
-  });
-});
-
-$('.main-main__search-form-select-input').on('keydown', function (event) {
-  if (event.keyCode === 40) {
-    currentFocus += 1;
-    addActive($(this).find('option'));
-  } else if (event.keyCode === 38) {
-    currentFocus -= 1;
-    addActive($(this).find('option'));
-  } else if (event.keyCode === 13) {
-    event.preventDefault();
-    if (currentFocus > -1) {
-      if ($(this).find('option')) $(this).find('option')[currentFocus].click();
-    }
-  }
 });
 
 $(document).click((event) => {
@@ -161,4 +183,13 @@ $(document).click((event) => {
     $('.main-main__search-form-select-datalist').fadeOut(1000);
   }
 });
+
 // ----------------------------------------------------------------//
+
+$('[data-open-block]').on('click', function () {
+  $('[data-open-block]').removeClass('active');
+  $(`[data-open-block="${$(this).data('open-block')}"`).addClass('active');
+
+  $('[data-block]').removeClass('active');
+  $(`[data-block="${$(this).data('open-block')}"`).addClass('active');
+});
---
+++ build/js/modal.js
@@ -0,0 +1,55 @@
+class Modal {
+  constructor(name) {
+    this.name = name;
+    this.modal = document.querySelector(`[data-modal="${name}"]`);
+    this.triggers = document.querySelectorAll(`[data-modal-el="${name}"]`);
+    this.body = document.querySelector('body');
+    this.openHendler();
+  }
+
+  open() {
+    this.modal.classList.remove('success', 'error');
+    this.modal.classList.add('active');
+    document.body.style.overflow = 'hidden';
+    this.modal.addEventListener('click', this.closeHendler);
+  }
+
+  close() {
+    this.modal.classList.remove('active');
+    document.body.style.overflow = 'unset';
+    this.modal.removeEventListener('click', this.closeHendler);
+  }
+
+  success() {
+    this.modal.classList.remove('error');
+    this.modal.classList.add('success');
+  }
+
+  error() {
+    this.modal.classList.remove('success');
+    this.modal.classList.add('error');
+  }
+
+  update() {
+    this.modal = document.querySelector(`[data-modal="${this.name}"]`);
+    this.triggers = document.querySelectorAll(`[data-modal-el="${this.name}"]`);
+    this.openHendler();
+  }
+
+  openHendler = () => {
+    this.triggers.forEach((item) => {
+      item.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.open();
+      });
+    });
+  };
+
+  closeHendler = (event) => {
+    if (event.target.classList.contains('close-x')) {
+      this.close();
+    }
+  };
+}
+
+const modalBrandInfo = document.querySelector('[data-modal="brand-info"]') ? new Modal('brand-info') : null;
