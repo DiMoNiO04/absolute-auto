9d257486e0b217d77d4dac78c47ff3205afc4aa1
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-08-02 12:04:13 +0300

    refactor: render content

--- build/js/render-content.js
+++ build/js/render-content.js
@@ -1,3 +1,14 @@
+const PAGES = {
+  SHIPING_PAYMENT: 'shiping-payment.html',
+  PERSONAL_CAB_IND: 'personal-cab-ind.html',
+  PERSONAL_CAB_END: 'personal-cab-ent.html',
+};
+
+const CONTENT = {
+  PAYMENT: 'payment',
+  PROFILE: 'profile',
+};
+
 function renderContentPage(elem) {
   $('[data-content]').removeClass('active');
   $(`[data-content=${elem}]`).addClass('active');
@@ -12,10 +23,15 @@ $(window).on('hashchange', () => {
 });
 
 $(document).ready(() => {
+  let locationArr = window.location.href.split('/');
+  let page = locationArr[locationArr.length - 1];
+
   if (window.location.hash) {
     const hash = window.location.hash.slice(1);
     renderContentPage(hash);
-  } else {
-    renderContentPage('payment');
+  } else if (page === PAGES.SHIPING_PAYMENT) {
+    renderContentPage(CONTENT.PAYMENT);
+  } else if (page === PAGES.PERSONAL_CAB_IND || page === PAGES.PERSONAL_CAB_END) {
+    renderContentPage(CONTENT.PROFILE);
   }
 });
