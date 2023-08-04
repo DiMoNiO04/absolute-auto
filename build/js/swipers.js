0d7d68af7864ebaa4b2f0058030a98ef0f6e9dfa
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-08-04 12:02:24 +0300

    feat: add category-go page

--- build/js/swipers.js
+++ build/js/swipers.js
@@ -4,7 +4,6 @@ const brandsSwiper = new Swiper('#brands-swiper', {
   grid: {
     rows: 2,
   },
-  spaceBetween: 30,
   navigation: {
     nextEl: '.main-brands__arrows-next',
     prevEl: '.main-brands__arrows-prev',
