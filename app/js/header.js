f416382720ece003654dc2c39d09428dba216e34
DiMoNiO04 <dima.razumov.940@mail.ru>
2023-08-13 22:26:03 +0300

    update

---
+++ app/js/header.js
@@ -0,0 +1,12 @@
+$('.menu-aside-mob-btn').click(() => {
+  $('.menu-aside-mob').addClass('active');
+});
+
+$('.menu-aside-mob-title__btn').click(() => {
+  $('.menu-aside-mob').removeClass('active');
+});
+
+$('.menu-aside-mob li').click(function () {
+  $('.menu-aside-mob').removeClass('active');
+  $('.menu-aside-mob-btn .menu-aside-mob__btn-name').text($(this).text());
+});
--- build/js/render-content.js
+++ build/js/render-content.js
@@ -19,13 +19,14 @@ function renderContentPage(elem) {
 
 function changeContent() {
   const locationArr = window.location.href.split('/');
-  const page = locationArr[locationArr.length - 1];
+  const page = locationArr[3];
 
   if (window.location.hash) {
     const hash = window.location.hash.slice(1);
     renderContentPage(hash);
   } else if (page === PAGES.SHIPING_PAYMENT) {
     renderContentPage(CONTENT.PAYMENT);
+    $('.menu-aside-mob-btn .menu-aside__btn-name').text($('.shiping__content .active .title').text());
   } else if (page === PAGES.PERSONAL_CAB_IND || page === PAGES.PERSONAL_CAB_END) {
     renderContentPage(CONTENT.PROFILE);
   }
@@ -33,3 +34,15 @@ function changeContent() {
 
 $(window).on('hashchange', () => changeContent());
 $(document).ready(() => changeContent());
+
+document.addEventListener('DOMContentLoaded', () => {
+  const locationArr = window.location.href.split('/');
+  const page = locationArr[3].split('#')[0];
+
+  if (page === PAGES.SHIPING_PAYMENT) {
+    $('.menu-aside-mob__btn-name').text($('.shiping__content.active').find('.title').text());
+  }
+  if (page === PAGES.PERSONAL_CAB_END || page === PAGES.PERSONAL_CAB_IND) {
+    $('.menu-aside-mob__btn-name').text($('.pers-cab__content.active').find('.title').text());
+  }
+});
